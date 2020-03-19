e9f13e8626eba76a0e5249143176005a3f07e141
kod3180 <kod3180@gmail.com>
2020-03-19 14:40:25 +0000

    context assessed using context.consumer approach

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
 
 class MusicList extends Component {
-
-  static contextType = ThemeContext;
-
   render() {
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div style={{background: theme.ui, color: theme.text}} className='music-list'>
-        <ul>
-          <li>Perfect</li>
-          <li>Afire Love</li>
-          <li>Love is not a fight</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              style={{ background: theme.bgcolor, color: theme.text }}
+              className="music-list"
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>Perfect</li>
+                <li style={{ background: theme.ui }}>Afire Love</li>
+                <li style={{ background: theme.ui }}>Love is not a fight</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
