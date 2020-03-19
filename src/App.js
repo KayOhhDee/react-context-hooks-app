86b68e44997fcb6ea69c58969b4de8184a2a2c6b
kod3180 <kod3180@gmail.com>
2020-03-19 20:59:28 +0000

    AuthContext created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navbar from './components/Navbar';
 import MusicList from './components/MusicList';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <MusicList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <MusicList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
