306011ef9096c8f59401301e5a9f134afdc7f414
kod3180 <kod3180@gmail.com>
2020-03-19 15:26:21 +0000

    toggler passed as a property to the context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,10 +8,18 @@ class ThemeContextProvider extends Component {
     light: {text: '#555', ui: '#ddd', bgcolor: '#eee'},
     dark: {text: '#ddd', ui: '#333', bgcolor: '#555'}
   }
+
+  toggleTheme = () => {
+    this.setState(prevState => {
+      return {
+        isLightTheme: !prevState.isLightTheme
+      }
+    })
+  }
   
   render() {
     return (
-      <ThemeContext.Provider value={{...this.state}}>
+      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
         {this.props.children}
       </ThemeContext.Provider>
     );
