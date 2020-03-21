87dda06bebae1da80731ebca0c5806c0ff1afc80
kod3180 <kod3180@gmail.com>
2020-03-21 11:44:56 +0000

    hooks useContext used to assess the context

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
 
-class MusicList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {context => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <div
-              style={{ background: theme.bgcolor, color: theme.text }}
-              className="music-list"
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>Perfect</li>
-                <li style={{ background: theme.ui }}>Afire Love</li>
-                <li style={{ background: theme.ui }}>Love is not a fight</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
+const MusicList = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      style={{ background: theme.bgcolor, color: theme.text }}
+      className="music-list"
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Perfect</li>
+        <li style={{ background: theme.ui }}>Afire Love</li>
+        <li style={{ background: theme.ui }}>Love is not a fight</li>
+      </ul>
+    </div>
+  );
 }
 
 export default MusicList;
