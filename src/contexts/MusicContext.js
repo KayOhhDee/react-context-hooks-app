2ed5a0fffd3a3f1ab0230ce1556a383578b48946
kod3180 <kod3180@gmail.com>
2020-03-26 12:28:47 +0000

    localStorage added to persist data

--- src/contexts/MusicContext.js
+++ src/contexts/MusicContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { musicReducer } from '../reducers/musicReducers';
 
 export const MusicContext = createContext();
 
 const MusicContextProvider = props => {
-  const [songs, dispatch] = useReducer(musicReducer, []);
+  const [songs, dispatch] = useReducer(musicReducer, [], () => {
+    const savedSongs = localStorage.getItem("rhs_app");
+    return  savedSongs ? JSON.parse(savedSongs) : [] 
+  });
+
+  useEffect(() => {
+    localStorage.setItem('rhs_app', JSON.stringify(songs))
+  }, [songs])
 
   return (
     <MusicContext.Provider value={{songs, dispatch}}>
